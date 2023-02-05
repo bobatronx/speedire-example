6734ced1e12913fc7f4b6bf93e26479df51c584a
bobatronx <jcefaratti@gmail.com>
2023-02-05 13:52:59 -0500

    Upgrade to latest version of speedire

--- pipeline/src/main.rs
+++ pipeline/src/main.rs
@@ -1,21 +1,20 @@
 use speedire::toolfs;
 use speedire::poetry_setup::Poetry;
 use speedire::kubectl_setup::Kubectl;
-use speedire::toolfs::{Tool, BuilderTool, DeployerTool};
+use speedire::toolfs::{BuilderTool, DeployerTool};
 
 fn main() {
     toolfs::initialize()
     .expect("unable to initialize");
     
-    let poetry = Poetry::default();
-    poetry.configure()
-    .and(poetry.build())
+    Poetry::default()
+    .build()
     .expect("unable to execute poetry build");
     
-    let kubectl = Kubectl::default();
-    kubectl.configure()
-    .and(kubectl.set_namespace("speedire"))
-    .and(kubectl.deploy(&["apply", "-f", "k8s/deployment.yaml"]))
+    Kubectl::default()
+    .namespace("speedire")
+    .apply("k8s/deployment.yaml")
+    .deploy()
     .expect("unable to execute kubectl");
 
     toolfs::cleanup()
