27b1f18081964de617f2ad212e5bbcdd3c5b553b
bobatronx <jcefaratti@gmail.com>
2023-02-05 21:01:40 -0500

    Implement new api with pipeline abstraction

--- pipeline/src/main.rs
+++ pipeline/src/main.rs
@@ -1,22 +1,37 @@
 use speedire::toolfs;
-use speedire::poetry_setup::Poetry;
-use speedire::kubectl_setup::Kubectl;
-use speedire::toolfs::{BuilderTool, DeployerTool};
+use speedire::poetry_setup::{PoetryCommandBuilder};
+use speedire::pipelines::{BuildPipelineBuilder, DeployPipelineBuilder};
+use speedire::kubectl_setup::{KubectlCommandBuilder};
+use speedire::pipelines::{PipelineBuilder, PipelineDeployer};
 
 fn main() {
     toolfs::initialize()
     .expect("unable to initialize");
     
-    Poetry::default()
+    // Create build command
+    let poetry_command = PoetryCommandBuilder::new()
+    .compile();
+
+    // Run Build
+    BuildPipelineBuilder::new()
+    .step(Box::new(poetry_command))
+    .compile()
     .build()
-    .expect("unable to execute poetry build");
-    
-    Kubectl::default()
+    .expect("build failed");
+
+    // Create deploy command
+    let kubectl_command = KubectlCommandBuilder::new()
     .namespace("speedire")
     .apply("k8s/deployment.yaml")
-    .deploy()
-    .expect("unable to execute kubectl");
+    .compile();
 
+    // Run Deploy
+    DeployPipelineBuilder::new()
+    .step(Box::new(kubectl_command))
+    .compile()
+    .deploy()
+    .expect("build failed");
+    
     toolfs::cleanup()
     .expect("unable to cleanup");
 }
